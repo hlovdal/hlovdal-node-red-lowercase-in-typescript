4e3761efdb6ce00a90eec4ef3d820f9e73f296be
Håkon Løvdal <kode@denkule.no>
2026-01-26 22:11:43 +0100

    Update script to support running node-red from repo

--- bin/run-node-red.sh
+++ bin/run-node-red.sh
@@ -2,12 +2,34 @@
 
 set -euo pipefail
 
-cd "$(dirname "$0")"/..
+USER_DIR=$(mktemp -d "${TMPDIR:-/tmp/}user-dir.XXXXXXXXXXXX")
+function cleanup {
+	rm -rf "$USER_DIR"
+}
+trap cleanup EXIT
 
-rm -fr user-dir
-mkdir user-dir
 
-cat > user-dir/.config.users.json <<EOF
+#REPO=$HOME/src/github/node-red
+
+# Avoid unbound error, convert not set to empty string
+set +u
+if [[ ! -n $REPO ]]
+then
+	REPO=""
+fi
+set -u
+
+if [[ -n $REPO ]]
+then
+	cd "$REPO"
+else
+	cd "$(dirname "$0")"/..
+fi
+
+rm -fr "$USER_DIR"
+mkdir "$USER_DIR"
+
+cat > "$USER_DIR"/.config.users.json <<EOF
 {
     "_": {
         "editor": {
@@ -20,11 +42,23 @@ cat > user-dir/.config.users.json <<EOF
 }
 EOF
 
-cat > user-dir/.config.runtime.json <<EOF
+cat > "$USER_DIR"/.config.runtime.json <<EOF
 {
     "telemetryEnabled": false
 }
 EOF
 
-npx node-red -p 1880 -u user-dir
+
+if [[ -n $REPO ]]
+then
+	if [[ $USER_DIR == *@* || $USER_DIR == *" "* ]]
+	then
+		echo "$0: Invalid USER_DIR value" 1>&2
+		exit 2
+	fi
+	sed -i "s@node packages/node_modules/node-red/red.js.*@node packages/node_modules/node-red/red.js -p 1880 -u $USER_DIR\",@" package.json
+	npm start
+else
+	npx node-red -p 1880 -u "$USER_DIR"
+fi
 
