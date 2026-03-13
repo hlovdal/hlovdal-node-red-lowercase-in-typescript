a92afcc54e6f558422f845b03dc7652c8dc04e31
Håkon Løvdal <kode@denkule.no>
2026-03-13 02:36:26 +0100

    Disable Cypress.Env
    Current runs complains:
    
    [CYPRESS] Warning: The allowCypressEnv configuration option is enabled. This allows any browser code to read values from Cypress.env(). This is insecure and will be removed in a future major version.
    [CYPRESS]
    [CYPRESS] 1. Replace Cypress.env() calls with cy.env() (for sensitive values) or Cypress.expose() (for public configuration)
    [CYPRESS] 2. Set allowCypressEnv: false in your Cypress configuration to disable Cypress.env()
    [CYPRESS]
    [CYPRESS] Learn more: https://on.cypress.io/cypress-env-migration
    
    https://docs.cypress.io/app/references/migration-guide#Migrating-away-from-Cypressenv

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+	allowCypressEnv: false,
 	e2e: {
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		setupNodeEvents(on, config) {
