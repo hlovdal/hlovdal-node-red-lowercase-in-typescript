a30725d853da0b350ac8fe1ea8fbee0d42f34d46
Håkon Løvdal <kode@denkule.no>
2026-06-05 23:47:57 +0200

    Specify shell for concurrently
    By default it would use cmd.exe for windows which in no way supports
    signals emitted from --kill-others. Before the e2e test would run
    successfully, but after sending termination signal to NODE-RED nothing
    happenes until the job times out:
    
        ...
        [CYPRESS]     ✔  All specs passed!                        01:18        1        1        -        -        -
        [CYPRESS]
        [CYPRESS] wait-on http://localhost:1880 && npm run cy:run exited with code 0
        --> Sending SIGTERM to other processes..
        Error: The operation was canceled.

--- bin/run-e2e.sh
+++ bin/run-e2e.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-concurrently --kill-others --success first -n 'NODE-RED,CYPRESS' -c 'green,blue' \
+concurrently --shell /bin/bash --kill-others --success first -n 'NODE-RED,CYPRESS' -c 'green,blue' \
 	"bash bin/run-node-red.sh" \
 	"wait-on http://localhost:1880 && npm run cy:run"
